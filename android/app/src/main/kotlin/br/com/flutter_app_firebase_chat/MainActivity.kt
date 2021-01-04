2ffafc856fb02d69bf0f9ae529a7d6084035d0cb
luidimso <luidi_mso@outlook.com>
2021-01-04 20:20:52 -0300

    Configuring firebase on project

---
+++ android/app/src/main/kotlin/br/com/flutter_app_firebase_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.flutter_app_firebase_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  Firestore.instance.collection("col").document("doc").setData({"text": "Luidi"});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
