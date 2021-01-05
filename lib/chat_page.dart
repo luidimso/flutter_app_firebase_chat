c8f7d769a2aea944a42d65079fba6d0805d13621
luidimso <luidi_mso@outlook.com>
2021-01-05 20:06:56 -0300

    Getting google user on login

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_firebase_chat/text_component.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -11,9 +13,22 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final GlobalKey<ScaffoldState> _scaffold = GlobalKey<ScaffoldState>();
+  final GoogleSignIn signin = GoogleSignIn();
+  FirebaseUser _user;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((event) {
+      _user = event;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffold,
       appBar: AppBar(
         title: Text("Hi"),
         elevation: 0,
@@ -52,7 +67,22 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   void _sendMessage({String text, File image}) async {
-    Map <String, dynamic> data = {};
+    final FirebaseUser user = await _login();
+
+    if(user == null) {
+      _scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Can't login. Try again."),
+          backgroundColor: Colors.red,
+        )
+      );
+    }
+
+    Map <String, dynamic> data = {
+      "uid": user.uid,
+      "name": user.displayName,
+      "profile": user.photoUrl
+    };
 
     if(text != null) {
       data["text"] = text;
@@ -66,5 +96,23 @@ class _ChatPageState extends State<ChatPage> {
     }
 
     Firestore.instance.collection("messages").add(data);
+  }
+
+  Future<FirebaseUser> _login() async {
+    if(_user != null) return _user;
+
+    try {
+      final GoogleSignInAccount account = await signin.signIn();
+      final GoogleSignInAuthentication auth = await account.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: auth.idToken,
+          accessToken: auth.accessToken
+      );
+      final AuthResult result = await FirebaseAuth.instance.signInWithCredential(credential);
+      final FirebaseUser user = result.user;
+      return user;
+    } catch(error) {
+      return null;
+    }
   }
 }
